992eced6ec764f8f39910d4c53e0b4a9b042092a
akhmadtaqiudin <akhmadtaqiudin@gmail.com>
2017-06-05 17:12:29 +0700

    penambahan modul penggunaan

---
+++ src/com/id/sima/modul/service/PenggunaanDao.java
@@ -0,0 +1,15 @@
+package com.id.sima.modul.service;
+
+import java.util.List;
+
+import com.id.sima.modul.model.Penggunaan;
+
+public interface PenggunaanDao {
+
+	int insertRecord(Penggunaan record);
+	int updateRecord(Penggunaan record);
+	int deletRecord(Penggunaan record);
+	int count(Penggunaan record);
+	Penggunaan selectWhereKode(Penggunaan row);
+	List<Penggunaan> selectAll(Penggunaan all);
+}
