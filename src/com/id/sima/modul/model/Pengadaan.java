9b894bda649058e9ea5b95a8fc066959cad4ddf3
akhmadtaqiudin <akhmadtaqiudin@gmail.com>
2017-06-05 17:10:52 +0700

    penambahan modul pengadaan

---
+++ src/com/id/sima/modul/model/Pengadaan.java
@@ -0,0 +1,41 @@
+package com.id.sima.modul.model;
+
+import java.util.Date;
+
+public class Pengadaan extends Barang{
+	private String kodePengadaan;
+	private String status;
+	private Date tanggal;
+	private String tglAwal;
+	private String tglAkhir;
+	public String getKodePengadaan() {
+		return kodePengadaan;
+	}
+	public void setKodePengadaan(String kodePengadaan) {
+		this.kodePengadaan = kodePengadaan;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Date getTanggal() {
+		return tanggal;
+	}
+	public void setTanggal(Date tanggal) {
+		this.tanggal = tanggal;
+	}
+	public String getTglAwal() {
+		return tglAwal;
+	}
+	public void setTglAwal(String tglAwal) {
+		this.tglAwal = tglAwal;
+	}
+	public String getTglAkhir() {
+		return tglAkhir;
+	}
+	public void setTglAkhir(String tglAkhir) {
+		this.tglAkhir = tglAkhir;
+	}
+}
