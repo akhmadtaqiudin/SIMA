bf0f8d730f7ec41e7039ddd03f80af9e472e3fa2
akhmadtaqiudin <akhmadtaqiudin@gmail.com>
2017-06-05 17:12:05 +0700

    penambahan modul pengadaan

---
+++ src/com/id/sima/modul/service/PengadaanDao.java
@@ -0,0 +1,15 @@
+package com.id.sima.modul.service;
+
+import java.util.List;
+
+import com.id.sima.modul.model.Pengadaan;
+
+public interface PengadaanDao {
+
+	int insertRecord(Pengadaan record);
+	int updateRecord(Pengadaan record);
+	int deletRecord(Pengadaan record);
+	int count(Pengadaan record);
+	Pengadaan selectWhereKode(Pengadaan row);
+	List<Pengadaan> selectAll(Pengadaan all);
+}
