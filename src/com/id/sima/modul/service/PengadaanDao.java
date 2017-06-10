36d66b3a5f188ee1f59519c8d9629965d45ef851
akhmadtaqiudin <akhmadtaqiudin@gmail.com>
2017-06-10 23:42:32 +0700

    modul pengadaan barang

--- src/com/id/sima/modul/service/PengadaanDao.java
+++ src/com/id/sima/modul/service/PengadaanDao.java
@@ -9,6 +9,7 @@ public interface PengadaanDao {
 	int insertRecord(Pengadaan record);
 	int updateRecord(Pengadaan record);
 	int deletRecord(Pengadaan record);
+	int maxID(Pengadaan record);
 	int count(Pengadaan record);
 	Pengadaan selectWhereKode(Pengadaan row);
 	List<Pengadaan> selectAll(Pengadaan all);
